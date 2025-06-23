298c7787a2c4352e307510c913d342b86198cc47
Yohan1220 <yjimenezh@unal.edu.co>
2025-06-23 11:04:45 -0500

    Update usuarios.java
    creacion clase usuarios

--- usuarios.java
+++ usuarios.java
@@ -1,2 +1,36 @@
 public class usuarios{
+    private int id;
+    private String nombre;
+    private String usuario;
+    private boolean citaDisponible;
+    private Cita proximaCita;
+
+    public usuarios(int id,String nombre,String usuario,boolean citaDisponible){
+        this.id=id;
+        this.nombre=nombre;
+        this.usuario=usuario;
+        this.boolean=citaDisponible;
+        this.Cita=new Cita;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    public String getUsuario(){
+        return usuario;
+    }
+    public int getId(){
+        return id;
+    }
+    public Cita getProxCita(){
+        return proximaCita;
+    }
+
+    public void consultar(){
+
+    }
+
+    public void agendar(){
+        
+    }
 }
