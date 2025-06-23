46343cbf4eb0772ed73d364c57d66fb63543d1b2
Yohan1220 <yjimenezh@unal.edu.co>
2025-06-23 11:10:37 -0500

    Create Profesor.java

---
+++ Profesor.java
@@ -0,0 +1,12 @@
+public class Profesor extends usuarios{
+    public Profesor(int id,String nombre,String usuario,boolean citaDisponible){
+        super();
+    }
+
+    public void cancelarCita(){
+        
+    }
+    public void anuncio(){
+        
+    }
+}
